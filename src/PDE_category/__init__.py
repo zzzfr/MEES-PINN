34ed585853020e1c84708f5c1756b550ad455d26
zzzfr <138281432+zzzfr@users.noreply.github.com>
2025-10-30 21:07:09 +0800

    Add files via upload

---
+++ src/PDE_category/__init__.py
@@ -0,0 +1,11 @@
+__all__ = [""]
+
+# from . import base
+# from . import Burgers
+# from . import chaotic
+# from . import Heat
+# from . import Helmholtz
+# from . import Inverse
+# from . import NS
+# from . import Poisson
+# from . import Wave
